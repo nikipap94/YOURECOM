c11064aa5d5dae411d82d1d3dfeb2cd30bf4ca3b
Silvana <sillvana.barboza@gmail.com>
2019-12-10 21:44:08 +0100

    Creating the activity course description.

---
+++ app/src/main/java/com/yourecom/ui/main/PageViewModel.java
@@ -0,0 +1,31 @@
+package com.yourecom.ui.main;
+
+import androidx.arch.core.util.Function;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Transformations;
+import androidx.lifecycle.ViewModel;
+
+public class PageViewModel extends ViewModel {
+
+    private MutableLiveData<Integer> mIndex = new MutableLiveData<>();
+    private LiveData<String> mText = Transformations.map(mIndex, new Function<Integer, String>() {
+        @Override
+        public String apply(Integer input) {
+
+            if(input == 1){
+                return "To do: Feedback List";
+            }else {
+                return "To do: Tips List";
+            }
+        }
+    });
+
+    public void setIndex(int index) {
+        mIndex.setValue(index);
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
